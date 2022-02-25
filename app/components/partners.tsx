4a4265788dff32a4928240ad3f20dd1a814f5364
Chaiwa Berian <chaiwa.berian@gmail.com>
2022-02-25 10:44:36 +0200

    Cleanups

---
+++ app/components/partners.tsx
@@ -0,0 +1,10 @@
+export default function Partners() {
+    return (
+      <div className="container max-w-5xl mx-auto m-8">
+        <h1 className="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">
+          Our Community Partners
+        </h1>
+      </div>
+    );
+  }
+  
---
+++ app/components/sponsors.tsx
@@ -0,0 +1,9 @@
+export default function Sponsors() {
+  return (
+    <div className="container max-w-5xl mx-auto m-8">
+      <h1 className="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">
+        Our Sponsors
+      </h1>
+    </div>
+  );
+}
