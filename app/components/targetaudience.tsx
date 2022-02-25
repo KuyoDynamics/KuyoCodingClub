c2ad7ec398109713f238180ddfb59551830ad33a
Chaiwa Berian <chaiwa.berian@gmail.com>
2022-02-25 11:30:31 +0200

    Add target audience component

---
+++ app/components/targetaudience.tsx
@@ -0,0 +1,14 @@
+export default function TargetAudience() {
+  return (
+    <>
+      <h1 className="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">
+        Aged 12-16 years?
+      </h1>
+      <p className="leading-normal text-2xl mb-8 text-black">
+        Awesome! You can become really good at computer programming, create
+        awesome Apps and games. We provide you with all the cool toolkits, and
+        help you meet/work with other tech savvy friends!
+      </p>
+    </>
+  );
+}
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -10,6 +10,7 @@ import ToolkitsList from "~/components/toolkitslist";
 import styles from "~/styles/index.css";
 import Partners from "~/components/partners";
 import Sponsors from "~/components/sponsors";
+import TargetAudience from "~/components/targetaudience";
 
 export function links() {
   return [{ rel: "stylesheet", href: styles }];
@@ -27,14 +28,7 @@ export default function Index() {
         <WaveBottomSVG />
       </div>
       <section className="bg-white border-b py-8">
-        <h1 className="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">
-          Aged 12-16 years?
-        </h1>
-        <p className="leading-normal text-2xl mb-8 text-black">
-          Awesome! You can become really good at computer programming, create
-          awesome Apps and games. We provide you with all the cool toolkits, and
-          help you meet/work with other tech savvy friends!
-        </p>
+        <TargetAudience />
       </section>
       <section className="bg-white border-b py-8">
         <ToolkitsList />
