d27eeb61f351b8bc81176425abc6781aa398b137
Chaiwa Berian <chaiwa.berian@gmail.com>
2022-03-02 22:02:10 +0200

    Add downarrow in target audience section, for #1

--- app/components/targetaudience.tsx
+++ app/components/targetaudience.tsx
@@ -1,8 +1,10 @@
+import DownArrow from "./svg/downarrow";
+
 export default function TargetAudience() {
   return (
     <>
       <div className="flex">
-        <div className="w-2/5  bg-white">
+        <div className="w-2/5  bg-white rounded-tr-2xl">
           <img src="children.svg" alt="children image" />
         </div>
         <div className="w-2/3">
@@ -11,17 +13,28 @@ export default function TargetAudience() {
           </h1>
           <h2 className="leading-normal font-bold text-2xl mb-8 text-white text-center p-1">
             Our <span className="text-green-500">Coding Club</span> is{" "}
-            <span className="text-blue-600">built for you</span>! We help you
-            learn
-            <span className="text-blue-500"> Computer Programming</span> , from
-            Beginner to Pro,{" "}
+            <span className="text-blue-600">built for you</span>! We{" "}
+            <span className="text-green-500">inspire</span> you to learn
+            <span className="text-blue-500"> Computer Programming</span> , from{" "}
+            <span className="text-yellow-300">Beginner</span> to{" "}
+            <span className="text-green-500">Pro</span>,{" "}
             <span className="text-yellow-300">with modern toolkits</span>!
-            Create{" "}
-            <span className="text-green-300">awesome Apps and games</span> like{" "}
-            <span className="text-blue-500">ABCD</span>! Be part of our tech
-            savvy community!
+            Create <span>awesome</span>{" "}
+            <span className="text-green-500">Apps</span> and{" "}
+            <span className="text-green-500">games</span> in{" "}
+            <span className="text-blue-500 tracking-widest">minutes</span>!
           </h2>
-          <img className="inline-block" src="lego.png" alt="lego.png" />
+          {/* Note: Here show a snippet of blocks on the left and equivalent text-based on the right! */}
+          {/* <pre>
+            <code>
+
+              if (true) &#123;
+                return  &quot;Yes &quot;;
+              &#125;
+             
+            </code>
+          </pre> */}
+          <DownArrow classNames={"animate-bounce h-6 w-6"} />
         </div>
       </div>
     </>
